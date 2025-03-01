4daec652928db8f9a35d116910005a8abdd90f72
jeanflo <jeanflorentcheveau@gmail.com>
2025-03-01 14:35:37 +0100

    Update iitc-plugin-export-links.meta.js

--- iitc-plugin-export-links.meta.js
+++ iitc-plugin-export-links.meta.js
@@ -3,7 +3,7 @@
 // @id         iitc-plugin-export-links
 // @name       IITC plugin: Export Portal Links
 // @category   Info
-// @version    0.4.5
+// @version    0.4.6
 // @namespace  https://github.com/jeanflo/iitc-plugin/blob/main/iitc-plugin-export-links
 // @updateURL  https://github.com/jeanflo/iitc-plugin/blob/main/export-links.user.js
 // @downloadURL https://github.com/jeanflo/iitc-plugin/blob/main/export-links.user.js
