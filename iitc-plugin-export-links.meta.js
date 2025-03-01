63d21fb804a86f0d8f6ae35d17fb817299064254
jeanflo <jeanflorentcheveau@gmail.com>
2025-03-01 14:32:05 +0100

    Create iitc-plugin-export-links.meta.js

---
+++ iitc-plugin-export-links.meta.js
@@ -0,0 +1,16 @@
+
+// ==UserScript==
+// @id         iitc-plugin-export-links
+// @name       IITC plugin: Export Portal Links
+// @category   Info
+// @version    0.4.5
+// @namespace  https://github.com/jeanflo/iitc-plugin/blob/main/iitc-plugin-export-links
+// @updateURL  https://github.com/jeanflo/iitc-plugin/blob/main/export-links.user.js
+// @downloadURL https://github.com/jeanflo/iitc-plugin/blob/main/export-links.user.js
+// @description Export links, mods, and resonators from a selected portal (including mod rarity).
+// @include        https://*.ingress.com/*
+// @include        http://*.ingress.com/*
+// @match          https://*.ingress.com/*
+// @match          http://*.ingress.com/*
+// @grant       none
+// ==/UserScript==
