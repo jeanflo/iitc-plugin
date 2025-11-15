9df256c4499da4b6da6927d75cf3bbd0fae42b75
jeanflo <jeanflorentcheveau@gmail.com>
2025-11-15 13:37:54 +0100

    Change plugin ID and update metadata details
    Updated metadata for the IITC plugin to reflect new functionality and links.

--- iitc-plugin-export-links.meta.js
+++ iitc-plugin-export-links.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @id         iitc-plugin-export-links
-// @name       IITC plugin: Export Portal Links
+// @id         iitc-plugin-portal-details-full
+// @name       IITC plugin: Portal Details Full
 // @category   Info
 // @version    1.6.4
-// @namespace  https://github.com/jeanflo/iitc-plugin/blob/main/iitc-plugin-export-links
-// @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/iitc-plugin-export-links.meta.js
-// @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/iitc-plugin-export-links.user.js
-// @description Export links, mods, and resonators from a selected portal (including mod rarity).
+// @namespace  https://github.com/jeanflo/iitc-plugin-portal-details-full
+// @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin-portal-details-full.meta.js
+// @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin-portal-details-full.user.js
+// @description Displays mods, resonators (level & owner), and linked portals (GUID name) for the selected portal. Telegram export is located next to the date and time. CSV export is also available.
 // @include        https://*.ingress.com/*
 // @include        http://*.ingress.com/*
 // @match          https://*.ingress.com/*
