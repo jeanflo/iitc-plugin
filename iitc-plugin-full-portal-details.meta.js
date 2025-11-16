ea0348b3b9d2b0282e19b61f02a4120696b30ca5
jeanflo <jeanflorentcheveau@gmail.com>
2025-11-16 11:12:22 +0100

    Update and rename iitc-plugin-export-links.meta.js to iitc-plugin-full-portal-details.meta.js

---
+++ iitc-plugin-full-portal-details.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id         iitc-plugin-full-portal-details
+// @name       IITC plugin: Full Portal Details
+// @category   Info
+// @version    2.4.1
+// @namespace  https://github.com/jeanflo/iitc-plugin-portal-details-full
+// @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.meta.js
+// @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.user.js
+// @description 2.4.1 Compatible Android! Affiche les mods, résonateurs et liens du portail sélectionné.
+// @match       https://intel.ingress.com/*
+// @match       http://intel.ingress.com/*
+// @grant       none
+// ==/UserScript==
