38f70c314fa53fb2264af6b11f5aadf9f4d1685e
jeanflo <jeanflorentcheveau@gmail.com>
2025-11-16 08:54:40 +0100

    Update iitc-plugin-export-links.user.js

--- iitc-plugin-export-links.user.js
+++ iitc-plugin-export-links.user.js
@@ -4,8 +4,8 @@
 // @category   Info
 // @version    1.6.7
 // @namespace  https://github.com/jeanflo/iitc-plugin-portal-details-full
-// @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin-portal-details-full.meta.js
-// @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin-portal-details-full.user.js
+// @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-export-links.meta.js
+// @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-export-links.user.js
 // @description 1.6.7 Affiche les mods, résonateurs (niveau & propriétaire), et les portails reliés (nom + GUID) du portail sélectionné. Bouton Telegram placé à côté de la date et heure. Export CSV/TXT/Excel désactivés sur mobile.
 // @include        https://*.ingress.com/*
 // @include        http://*.ingress.com/*
