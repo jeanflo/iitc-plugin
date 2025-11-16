156bde816603239a6918f80314154e67d38e6114
jeanflo <jeanflorentcheveau@gmail.com>
2025-11-16 10:39:56 +0100

    Update iitc-plugin-export-links.user.js

--- iitc-plugin-export-links.user.js
+++ iitc-plugin-export-links.user.js
@@ -2,11 +2,11 @@
 // @id         iitc-plugin-full-portal-details
 // @name       IITC plugin: Full Portal Details
 // @category   Info
-// @version    2.0.0
+// @version    2.1.0
 // @namespace  https://github.com/jeanflo/iitc-plugin-portal-details-full
 // @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-export-links.meta.js
 // @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-export-links.user.js
-// @description 2.0.0 Compatible Android! Affiche les mods, résonateurs (niveau & propriétaire), et les portails reliés (nom + GUID) du portail sélectionné. Bouton Telegram placé à côté de la date et heure. Export CSV/TXT/Excel désactivés sur mobile.
+// @description 2.1.0 Compatible Android! Affiche les mods, résonateurs (niveau & propriétaire), et les portails reliés (nom + GUID) du portail sélectionné. Bouton Telegram placé à côté de la date et heure. Export CSV/TXT/Excel désactivés sur mobile.
 // @include        https://*.ingress.com/*
 // @include        http://*.ingress.com/*
 // @match          https://*.ingress.com/*
@@ -15,61 +15,62 @@
 // ==/UserScript==
 
 function wrapper(plugin_info) {
-// ensure plugin framework is there, even if iitc is not yet loaded
-if(typeof window.plugin !== 'function') window.plugin = function() {};
+if(typeof window.plugin != 'function') window.plugin = function() {};
 
-// PLUGIN START
 window.plugin.portalDetailsFull = function() {};
 
 window.plugin.portalDetailsFull.currentData = null;
 
 window.plugin.portalDetailsFull.isMobile = function() {
-    return window.useAndroidPanes() || /Mobile|Android/i.test(navigator.userAgent);
+    if (window.useAndroidPanes) {
+        return window.useAndroidPanes();
+    }
+    return false;
 };
 
 window.plugin.portalDetailsFull.exportTelegram = function() {
     var data = window.plugin.portalDetailsFull.currentData;
     if (!data) return;
-
+    
     var txt = '';
     var i;
-
-    txt += '[Portal] **' + data.name + '**\n';
-    txt += '[GUID] ' + data.guid + '\n\n';
-
-    txt += '**Mods:**\n';
+    
+    txt = txt + '[Portal] **' + data.name + '**\n';
+    txt = txt + '[GUID] ' + data.guid + '\n\n';
+    
+    txt = txt + '**Mods:**\n';
     if (data.mods.length > 0) {
         for (i = 0; i < data.mods.length; i++) {
-            txt += '  - **' + data.mods[i].name + '** (' + data.mods[i].owner + ', ' + data.mods[i].rarity + ')\n';
+            txt = txt + '  - **' + data.mods[i].name + '** (' + data.mods[i].owner + ', ' + data.mods[i].rarity + ')\n';
         }
     } else {
-        txt += '  - None\n';
+        txt = txt + '  - None\n';
     }
-
-    txt += '\n**Resonators:**\n';
+    
+    txt = txt + '\n**Resonators:**\n';
     if (data.resonators.length > 0) {
         for (i = 0; i < data.resonators.length; i++) {
-            txt += '  - **L' + data.resonators[i].level + '** (' + data.resonators[i].owner + ')\n';
+            txt = txt + '  - **L' + data.resonators[i].level + '** (' + data.resonators[i].owner + ')\n';
         }
     } else {
-        txt += '  - None\n';
+        txt = txt + '  - None\n';
     }
-
-    txt += '\n**Linked portals:**\n';
+    
+    txt = txt + '\n**Linked portals:**\n';
     if (data.links.length > 0) {
         for (i = 0; i < data.links.length; i++) {
-            txt += '  - ' + data.links[i].name + '\n';
-            txt += 'https://link.ingress.com/portal/' + data.links[i].guid + '\n\n';
+            txt = txt + '  - ' + data.links[i].name + '\n';
+            txt = txt + 'https://link.ingress.com/portal/' + data.links[i].guid + '\n\n';
         }
     } else {
-        txt += '  - None\n';
+        txt = txt + '  - None\n';
     }
-
+    
     if (navigator.clipboard && navigator.clipboard.writeText) {
         navigator.clipboard.writeText(txt).then(function() {
-            alert('Data copied to clipboard!');
+            alert('Data copied!');
         }).catch(function() {
-            alert('Error: Unable to copy');
+            alert('Error copying');
         });
     } else {
         alert('Clipboard not available');
@@ -78,114 +79,128 @@ window.plugin.portalDetailsFull.exportTelegram = function() {
 
 window.plugin.portalDetailsFull.showDialog = function() {
     if (!window.selectedPortal) return;
-
+    
     var portal = window.portals[window.selectedPortal];
-    if (!portal || !portal.options.data) {
-        alert('Unable to load portal details');
-        return;
-    }
-
+    if (!portal) return;
+    if (!portal.options) return;
+    if (!portal.options.data) return;
+    
     var data = portal.options.data;
     var guid = window.selectedPortal;
-    var name = data.title || 'Unknown Portal';
-    var mods = data.mods || [];
-    var resonators = data.resonators || [];
-
+    var name = data.title;
+    if (!name) name = 'Unknown Portal';
+    
+    var mods = data.mods;
+    if (!mods) mods = [];
+    
+    var resonators = data.resonators;
+    if (!resonators) resonators = [];
+    
     var processedMods = [];
     var processedRes = [];
     var linkedPortals = [];
-
+    
     var i;
     for (i = 0; i < mods.length; i++) {
         if (mods[i]) {
-            processedMods.push({
-                name: mods[i].name || 'Unknown Mod',
-                owner: mods[i].owner || 'Unknown',
-                rarity: mods[i].rarity || 'Unknown'
-            });
+            var modObj = {};
+            modObj.name = mods[i].name;
+            if (!modObj.name) modObj.name = 'Unknown Mod';
+            modObj.owner = mods[i].owner;
+            if (!modObj.owner) modObj.owner = 'Unknown';
+            modObj.rarity = mods[i].rarity;
+            if (!modObj.rarity) modObj.rarity = 'Unknown';
+            processedMods.push(modObj);
         }
     }
-
+    
     for (i = 0; i < resonators.length; i++) {
         if (resonators[i]) {
-            processedRes.push({
-                level: resonators[i].level || '?',
-                owner: resonators[i].owner || 'Unknown'
-            });
+            var resObj = {};
+            resObj.level = resonators[i].level;
+            if (!resObj.level) resObj.level = '?';
+            resObj.owner = resonators[i].owner;
+            if (!resObj.owner) resObj.owner = 'Unknown';
+            processedRes.push(resObj);
         }
     }
-
-    for (var linkId in window.links) {
-        var link = window.links[linkId];
+    
+    var allLinks = window.links;
+    for (var linkId in allLinks) {
+        var link = allLinks[linkId];
         var linkData = link.options.data;
-
-        if (linkData.oGuid === guid || linkData.dGuid === guid) {
-            var targetGuid = (linkData.oGuid === guid) ? linkData.dGuid : linkData.oGuid;
+        
+        var isOrigin = (linkData.oGuid == guid);
+        var isDest = (linkData.dGuid == guid);
+        
+        if (isOrigin || isDest) {
+            var targetGuid = isOrigin ? linkData.dGuid : linkData.oGuid;
             var targetPortal = window.portals[targetGuid];
-
-            if (targetPortal && targetPortal.options.data) {
-                linkedPortals.push({
-                    name: targetPortal.options.data.title || 'Unknown',
-                    guid: targetGuid
-                });
+            
+            if (targetPortal && targetPortal.options && targetPortal.options.data) {
+                var linkObj = {};
+                linkObj.name = targetPortal.options.data.title;
+                if (!linkObj.name) linkObj.name = 'Unknown';
+                linkObj.guid = targetGuid;
+                linkedPortals.push(linkObj);
             }
         }
     }
-
-    window.plugin.portalDetailsFull.currentData = {
-        name: name,
-        guid: guid,
-        mods: processedMods,
-        resonators: processedRes,
-        links: linkedPortals
-    };
-
+    
+    var saveData = {};
+    saveData.name = name;
+    saveData.guid = guid;
+    saveData.mods = processedMods;
+    saveData.resonators = processedRes;
+    saveData.links = linkedPortals;
+    window.plugin.portalDetailsFull.currentData = saveData;
+    
     var now = new Date().toLocaleString();
     var html = '<div>';
-
-    html += '<div style="margin-bottom:10px;">';
-    html += '<span style="font-size:11px;color:#666;">' + now + '</span> ';
-    html += '<button onclick="window.plugin.portalDetailsFull.exportTelegram();return false;" style="padding:6px 12px;background:#3874ff;color:white;border:none;border-radius:3px;cursor:pointer;float:right;">Telegram Export</button>';
-    html += '</div>';
-
-    html += '<h3 style="color:#ffce00;">' + name + '</h3>';
-    html += '<p style="font-size:11px;color:#888;">GUID: ' + guid + '</p>';
-
-    html += '<h4 style="border-bottom:1px solid #333;padding-bottom:3px;">Mods</h4>';
+    
+    html = html + '<div style="margin-bottom:10px;">';
+    html = html + '<span style="font-size:11px;color:#666;">' + now + '</span> ';
+    html = html + '<button onclick="window.plugin.portalDetailsFull.exportTelegram();return false;" style="padding:6px 12px;background:#3874ff;color:white;border:none;border-radius:3px;cursor:pointer;float:right;">Telegram</button>';
+    html = html + '</div>';
+    
+    html = html + '<h3 style="color:#ffce00;">' + name + '</h3>';
+    html = html + '<p style="font-size:11px;color:#888;">GUID: ' + guid + '</p>';
+    
+    html = html + '<h4 style="border-bottom:1px solid #333;">Mods</h4>';
     if (processedMods.length > 0) {
-        html += '<ul>';
+        html = html + '<ul>';
         for (i = 0; i < processedMods.length; i++) {
-            html += '<li><b>' + processedMods[i].name + '</b> (Owner: ' + processedMods[i].owner + ', Rarity: ' + processedMods[i].rarity + ')</li>';
+            html = html + '<li><b>' + processedMods[i].name + '</b> (' + processedMods[i].owner + ', ' + processedMods[i].rarity + ')</li>';
         }
-        html += '</ul>';
+        html = html + '</ul>';
     } else {
-        html += '<p style="color:#888;font-style:italic;">None</p>';
+        html = html + '<p style="color:#888;">None</p>';
     }
-
-    html += '<h4 style="border-bottom:1px solid #333;padding-bottom:3px;">Resonators</h4>';
+    
+    html = html + '<h4 style="border-bottom:1px solid #333;">Resonators</h4>';
     if (processedRes.length > 0) {
-        html += '<ul>';
+        html = html + '<ul>';
         for (i = 0; i < processedRes.length; i++) {
-            html += '<li><b>Level ' + processedRes[i].level + '</b> (Owner: ' + processedRes[i].owner + ')</li>';
+            html = html + '<li><b>Level ' + processedRes[i].level + '</b> (' + processedRes[i].owner + ')</li>';
         }
-        html += '</ul>';
+        html = html + '</ul>';
     } else {
-        html += '<p style="color:#888;font-style:italic;">None</p>';
+        html = html + '<p style="color:#888;">None</p>';
     }
-
-    html += '<h4 style="border-bottom:1px solid #333;padding-bottom:3px;">Linked Portals</h4>';
+    
+    html = html + '<h4 style="border-bottom:1px solid #333;">Linked Portals</h4>';
     if (linkedPortals.length > 0) {
-        html += '<ul>';
+        html = html + '<ul>';
         for (i = 0; i < linkedPortals.length; i++) {
-            html += '<li><b>' + linkedPortals[i].name + '</b><br><span style="font-size:10px;color:#888;">' + linkedPortals[i].guid + '</span></li>';
+            html = html + '<li><b>' + linkedPortals[i].name + '</b><br><span style="font-size:10px;color:#888;">' + linkedPortals[i].guid + '</span></li>';
         }
-        html += '</ul>';
+        html = html + '</ul>';
     } else {
-        html += '<p style="color:#888;font-style:italic;">None</p>';
+        html = html + '<p style="color:#888;">None</p>';
     }
-
-    html += '</div>';
-
+    
+    html = html + '</div>';
+    
     dialog({
         title: 'Full Portal Details',
         html: html,
@@ -195,15 +210,15 @@ window.plugin.portalDetailsFull.showDialog = function() {
 
 window.plugin.portalDetailsFull.addButton = function() {
     if (!window.selectedPortal) return;
-
+    
     var container = document.getElementById('portaldetails');
     if (!container) return;
-
+    
     var existingBtn = document.getElementById('portal-details-btn');
     if (existingBtn) {
         existingBtn.parentNode.removeChild(existingBtn);
     }
-
+    
     var btn = document.createElement('a');
     btn.id = 'portal-details-btn';
     btn.href = '#';
@@ -218,13 +233,13 @@ window.plugin.portalDetailsFull.addButton = function() {
     btn.style.textAlign = 'center';
     btn.style.cursor = 'pointer';
     btn.style.fontWeight = 'bold';
-
+    
     btn.addEventListener('click', function(e) {
         e.preventDefault();
         window.plugin.portalDetailsFull.showDialog();
         return false;
     });
-
+    
     var linkDetails = container.querySelector('.linkdetails');
     if (linkDetails) {
         linkDetails.appendChild(btn);
@@ -241,18 +256,15 @@ var setup = function() {
     });
 };
 
-// PLUGIN END
-
 setup.info = plugin_info;
 if(!window.bootPlugins) window.bootPlugins = [];
 window.bootPlugins.push(setup);
-if(window.iitcLoaded && typeof setup === 'function') setup();
+if(window.iitcLoaded && typeof setup == 'function') setup();
 }
 
-// WRAPPER END
 var script = document.createElement('script');
 var info = {};
-if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
+if (typeof GM_info != 'undefined' && GM_info && GM_info.script) {
     info.script = {
         version: GM_info.script.version,
         name: GM_info.script.name,
