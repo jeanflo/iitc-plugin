c3ae71f15baa60628584fc2b5462d14643f7dd15
jeanflo <jeanflorentcheveau@gmail.com>
2025-11-16 11:30:13 +0100

    Update iitc-plugin-full-portal-details.meta.js

--- iitc-plugin-full-portal-details.meta.js
+++ iitc-plugin-full-portal-details.meta.js
@@ -2,11 +2,11 @@
 // @id         iitc-plugin-full-portal-details
 // @name       IITC plugin: Full Portal Details
 // @category   Info
-// @version    3.0.0
+// @version    3.0.1
 // @namespace  https://github.com/jeanflo/iitc-plugin-portal-details-full
 // @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.meta.js
 // @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.user.js
-// @description 3.0.0 Compatible Android! Affiche les mods, résonateurs et liens du portail sélectionné.
+// @description 3.0.1 Compatible Android! Affiche les mods, résonateurs et liens du portail sélectionné.
 // @match       https://intel.ingress.com/*
 // @match       http://intel.ingress.com/*
 // @grant       none
