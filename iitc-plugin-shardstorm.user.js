c770a6186f0f14e08f3a83b0f3dac1ac8a7dee82
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-04 19:23:25 +0100

    Update iitc-plugin-shardstorm.user.js

--- iitc-plugin-shardstorm.user.js
+++ iitc-plugin-shardstorm.user.js
@@ -4,8 +4,8 @@
 // @category       Layer
 // @version        1.1.2
 // @namespace      https://github.com/jeanflo/iitc-plugin
-// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/shardstorm.meta.js
-// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/shardstorm.user.js
+// @updateURL      https://github.com/jeanflo/iitc-plugin/raw/bbeeec5e93e2f193fc654cce8c96a0b54b4e1e90/iitc-plugin-shardstorm.meta.js
+// @downloadURL    https://github.com/jeanflo/iitc-plugin/raw/bbeeec5e93e2f193fc654cce8c96a0b54b4e1e90/iitc-plugin-shardstorm.user.js
 // @description    Affiche les zones tactiques (1-3-5km) avec gestion individuelle des calques. Optimisé Mobile.
 // @include        https://intel.ingress.com/*
 // @include        http://*.ingress.com/intel*
