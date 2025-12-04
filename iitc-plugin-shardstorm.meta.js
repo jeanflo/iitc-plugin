f7f53d5f95bda370145c861210e55e166ef97c93
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-04 18:34:25 +0100

    Create iitc-plugin-shardstorm.meta.js

---
+++ iitc-plugin-shardstorm.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             iitc-plugin-shardstorm
+// @name           IITC plugin: ShardStorm
+// @category       Layer
+// @version        1.0.0
+// @namespace      https://github.com/jeanflo/iitc-plugin
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/shardstorm.meta.js
+// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/shardstorm.user.js
+// @description    Affiche les zones tactiques (1-3-5km) autour du portail sélectionné.
+// @include        https://intel.ingress.com/*
+// @include        http://*.ingress.com/intel*
+// @match          https://intel.ingress.com/*
+// @match          https://intel.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
