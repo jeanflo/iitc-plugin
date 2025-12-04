c2b44f35e955d31dbcb669cfb81824e341d37bb3
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-04 20:23:34 +0100

    Update iitc-plugin-full-portal-details.user.js

--- iitc-plugin-full-portal-details.user.js
+++ iitc-plugin-full-portal-details.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @id             iitc-plugin-full-portal-details
-// @name           IITC plugin: Full Portal Details
-// @category       Portal Info
-// @author         Z0mZ0m
-// @version        3.1.4
-// @namespace      https://github.com/jeanflo/iitc-plugin-portal-details-full
-// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.meta.js
-// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.user.js
-// @description    3.1.4 Compatible Android! Affiche les mods, résonateurs et liens du portail. Exports CSV/TXT/Telegram.
-// @match          https://intel.ingress.com/*
-// @match          http://intel.ingress.com/*
-// @grant          none
+// @id         iitc-plugin-full-portal-details
+// @name       IITC plugin: Full Portal Details
+// @category   Portal Info
+// @author     Z0mZ0m
+// @version    3.1.4
+// @namespace  https://github.com/jeanflo/iitc-plugin-portal-details-full
+// @updateURL  https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.meta.js
+// @downloadURL https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-full-portal-details.user.js
+// @description 3.1.4 Compatible Android! Affiche les mods, résonateurs et liens du portail sélectionné. Exports CSV/TXT/Excel/Telegram.
+// @match       https://intel.ingress.com/*
+// @match       http://intel.ingress.com/*
+// @grant       none
 // ==/UserScript==
 
 var info = {};
