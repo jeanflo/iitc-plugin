aa469abb0cd918c188e1166cc95800b9284f43c5
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-04 18:46:08 +0100

    Update iitc-plugin-shardstorm.user.js

--- iitc-plugin-shardstorm.user.js
+++ iitc-plugin-shardstorm.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-shardstorm
 // @name           IITC plugin: ShardStorm
 // @category       Layer
-// @version        1.0.2
+// @version        1.1.0
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/shardstorm.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/main/shardstorm.user.js
-// @description    Affiche les zones tactiques (1-3-5km) autour du portail sélectionné.
+// @description    Affiche les zones tactiques (1-3-5km) avec gestion individuelle des calques.
 // @include        https://intel.ingress.com/*
 // @include        http://*.ingress.com/intel*
 // @match          https://intel.ingress.com/*
@@ -18,12 +18,17 @@ function wrapper(plugin_info) {
     if(typeof window.plugin !== 'function') window.plugin = function() {};
 
     plugin_info.buildName = 'iitc-plugin-shardstorm';
-    plugin_info.dateTimeVersion = '202312040012';
+    plugin_info.dateTimeVersion = '202312040015';
     plugin_info.pluginId = 'shardstorm';
 
     // Initialisation
     window.plugin.shardstorm = {};
-    window.plugin.shardstorm.layerGroup = null;
+    // On crée un objet pour stocker nos 3 calques distincts
+    window.plugin.shardstorm.layers = {
+        zone1: null,
+        zone3: null,
+        zone5: null
+    };
     window.plugin.shardstorm.activeGuid = null;
 
     // --- 1. LOGIQUE DE DESSIN ---
@@ -32,58 +37,63 @@ function wrapper(plugin_info) {
 
         if (!guid) return;
 
-        // Si actif sur ce portail -> On éteint
+        // Si actif sur ce portail -> On éteint tout
         if (window.plugin.shardstorm.activeGuid === guid) {
             window.plugin.shardstorm.clear();
             return;
         }
 
-        // Sinon on dessine
+        // Sinon on nettoie d'abord et on dessine
         window.plugin.shardstorm.clear();
 
         var portal = window.portals[guid];
         var latLng = portal ? portal.getLatLng() : null;
         if (!latLng) return;
 
-        // Cercles (Rayons en mètres)
-        var circles = [
-            { radius: 5000, color: '#FF0000', title: '5km' },
-            { radius: 3000, color: '#00FF00', title: '3km' },
-            { radius: 1000, color: '#FF0000', title: '1km' }
-        ];
+        // Options communes pour le style
+        var commonStyle = {
+            fillOpacity: 0.05,
+            weight: 1,
+            interactive: false
+        };
 
-        circles.forEach(function(c) {
-            var circle = L.circle(latLng, c.radius, {
-                color: c.color,
-                fillColor: c.color,
-                fillOpacity: 0.05, // Opacité très légère pour ne pas gêner
-                weight: 1,         // Trait fin
-                interactive: false
-            });
-            window.plugin.shardstorm.layerGroup.addLayer(circle);
-        });
+        // Dessin du cercle 1km
+        L.circle(latLng, 1000, $.extend({}, commonStyle, { color: '#FF0000', fillColor: '#FF0000' }))
+         .addTo(window.plugin.shardstorm.layers.zone1);
+
+        // Dessin du cercle 3km
+        L.circle(latLng, 3000, $.extend({}, commonStyle, { color: '#00FF00', fillColor: '#00FF00' }))
+         .addTo(window.plugin.shardstorm.layers.zone3);
+
+        // Dessin du cercle 5km
+        L.circle(latLng, 5000, $.extend({}, commonStyle, { color: '#FF0000', fillColor: '#FF0000' }))
+         .addTo(window.plugin.shardstorm.layers.zone5);
 
         window.plugin.shardstorm.activeGuid = guid;
         window.plugin.shardstorm.updateButton(true);
     };
 
     window.plugin.shardstorm.clear = function() {
-        window.plugin.shardstorm.layerGroup.clearLayers();
+        // On vide les 3 calques
+        window.plugin.shardstorm.layers.zone1.clearLayers();
+        window.plugin.shardstorm.layers.zone3.clearLayers();
+        window.plugin.shardstorm.layers.zone5.clearLayers();
+
         window.plugin.shardstorm.activeGuid = null;
         window.plugin.shardstorm.updateButton(false);
     };
 
-    // --- 2. INTERFACE UTILISATEUR ---
+    // --- 2. INTERFACE UTILISATEUR (Bouton Portail) ---
     window.plugin.shardstorm.updateButton = function(isActive) {
         var btn = $('#shardstorm-btn');
         if (btn.length) {
             if (isActive) {
                 btn.text('ShardStorm: ON')
-                   .css('color', '#ffce00') // Jaune standard IITC pour actif
+                   .css('color', '#ffce00')
                    .css('font-weight', 'bold');
             } else {
                 btn.text('ShardStorm: Off')
-                   .css('color', '') // Retour couleur lien par défaut
+                   .css('color', '')
                    .css('font-weight', 'normal');
             }
         }
@@ -95,9 +105,8 @@ function wrapper(plugin_info) {
 
         $('#shardstorm-aside').remove();
 
-        // Utilisation d'un simple <a> dans un <aside> pour s'intégrer nativement
         var aside = $('<aside id="shardstorm-aside"></aside>');
-        var btn = $('<a id="shardstorm-btn" href="#" onclick="return false;" title="Afficher les zones 1/3/5km">ShardStorm: Off</a>');
+        var btn = $('<a id="shardstorm-btn" href="#" onclick="return false;" title="Calculer les zones">ShardStorm: Off</a>');
 
         if (window.plugin.shardstorm.activeGuid === guid) {
             btn.text('ShardStorm: ON')
@@ -111,16 +120,22 @@ function wrapper(plugin_info) {
         });
 
         aside.append(btn);
-
-        // Ajout dans la section linkdetails (là où sont les autres plugins)
         $('.linkdetails').append(aside);
     };
 
     var setup = function() {
-        window.plugin.shardstorm.layerGroup = new L.LayerGroup();
-        window.map.addLayer(window.plugin.shardstorm.layerGroup);
+        // Création des groupes de calques
+        window.plugin.shardstorm.layers.zone1 = new L.LayerGroup();
+        window.plugin.shardstorm.layers.zone3 = new L.LayerGroup();
+        window.plugin.shardstorm.layers.zone5 = new L.LayerGroup();
 
-        // CSS minimal juste pour l'espacement, sans couleurs forcées
+        // Ajout au gestionnaire de calques officiel IITC (Menu Layers)
+        // Le 'true' à la fin signifie qu'ils sont cochés par défaut
+        window.addLayerGroup('ShardStorm (1km)', window.plugin.shardstorm.layers.zone1, true);
+        window.addLayerGroup('ShardStorm (3km)', window.plugin.shardstorm.layers.zone3, true);
+        window.addLayerGroup('ShardStorm (5km)', window.plugin.shardstorm.layers.zone5, true);
+
+        // CSS minimal
         $('<style>').prop('type', 'text/css').html(`
             #shardstorm-aside {
                 display: block;
@@ -129,7 +144,7 @@ function wrapper(plugin_info) {
         `).appendTo('head');
 
         window.addHook('portalDetailsUpdated', window.plugin.shardstorm.onPortalSelected);
-        console.log('[ShardStorm] Plugin loaded.');
+        console.log('[ShardStorm] Plugin loaded with multi-layers.');
     };
 
     setup.info = plugin_info;
