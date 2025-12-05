4576f9c605a8d82df16ad2701cbf0555cb26fb9d
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-05 12:59:18 +0100

    Update iitc-plugin-shardstorm.meta.js

--- iitc-plugin-shardstorm.meta.js
+++ iitc-plugin-shardstorm.meta.js
@@ -3,7 +3,7 @@
 // @name           IITC plugin: ShardStorm
 // @category       Anomaly
 // @author         Z0mZ0m
-// @version        1.2.2
+// @version        1.2.3
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-shardstorm.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-shardstorm.user.js
