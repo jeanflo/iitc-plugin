0bfb96100bef53e23958f027c432f16d66224313
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-06 18:35:02 +0100

    Update iitc-plugin-shardstorm.meta.js

--- iitc-plugin-shardstorm.meta.js
+++ iitc-plugin-shardstorm.meta.js
@@ -7,7 +7,7 @@
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-shardstorm.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-shardstorm.user.js
-// @description    Affiche les zones tactiques + Export + Traduction (Interface Native).
+// @description    Affiche les zones tactiques + Export + Liste (Noms de zones colorés).
 // @include        https://intel.ingress.com/*
 // @include        http://*.ingress.com/intel*
 // @match          https://intel.ingress.com/*
