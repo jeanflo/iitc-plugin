1d47b69c40910d6d1df6c0d615186bb837f03c50
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-30 14:17:53 +0100

    Update iitc-plugin-layer-count-custom.meta.js

--- iitc-plugin-layer-count-custom.meta.js
+++ iitc-plugin-layer-count-custom.meta.js
@@ -2,7 +2,7 @@
 // @author         Z0mz0m
 // @name           IITC plugin: Layer count & Persistent Global Highlighter
 // @category       Info
-// @version        1.1.0
+// @version        1.1.1
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.user.js
 // @description    Surligne les fields de façon persistante au clic et sur l'écran
