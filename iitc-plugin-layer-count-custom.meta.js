282d3cb6dfcb3f94865acb6721cd111f9735aa30
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-30 14:01:15 +0100

    Add metadata for IITC Layer Count plugin

---
+++ iitc-plugin-layer-count-custom.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         Z0mz0m
+// @name           IITC plugin: Layer count & Persistent Global Highlighter
+// @category       Info
+// @version        1.1.0
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.meta.js
+// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.user.js
+// @description    Surligne les fields de façon persistante au clic et sur l'écran
+// @id             iitc-plugin-layer-count-custom
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @grant          none
+// ==/UserScript==
