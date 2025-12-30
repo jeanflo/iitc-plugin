6e4fd37de35a4128fde0bece3f9638e568599d73
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-30 14:00:18 +0100

    Fix update and download URLs in metadata

--- Iitc-plugin-layer-count-custom.user.js
+++ Iitc-plugin-layer-count-custom.user.js
@@ -3,7 +3,7 @@
 // @name           IITC plugin: Layer count & Persistent Global Highlighter
 // @category       Info
 // @version        1.1.0
-// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-shardstorm.meta.js
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.user.js
 // @description    Surligne les fields de façon persistante au clic et sur l'écran
 // @id             iitc-plugin-layer-count-custom
