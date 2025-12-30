19eefe5406323158ac1e6a436e3771cc48dcdc2f
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-30 14:17:37 +0100

    Update iitc-plugin-layer-count-custom.user.js

--- iitc-plugin-layer-count-custom.user.js
+++ iitc-plugin-layer-count-custom.user.js
@@ -2,7 +2,7 @@
 // @author         Z0mz0m
 // @name           IITC plugin: Layer count & Persistent Global Highlighter
 // @category       Info
-// @version        1.1.0
+// @version        1.1.1
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.user.js
 // @description    Surligne les fields de façon persistante au clic et sur l'écran
@@ -18,99 +18,82 @@ function wrapper(plugin_info) {
   var layerCount = {};
   window.plugin.layerCount = layerCount;
   var tooltip;
-  var currentHitFields = []; // Stocke les fields allumés par le clic
+  var currentHitFields = [];
 
-  // --- FONCTION DE RESET (RETOUR AUX COULEURS D'ORIGINE) ---
+  // --- RÉCUPÉRATION ET CONVERSION DU POLYGONE DRAWTOOLS ---
+  function getPolygonPoints() {
+    if (!window.plugin.drawTools || !window.plugin.drawTools.drawnItems) return null;
+    
+    var layers = window.plugin.drawTools.drawnItems.getLayers();
+    for (var i = 0; i < layers.length; i++) {
+      var layer = layers[i];
+      if (layer instanceof L.Polygon) {
+        var latlngs = layer.getLatLngs();
+        // DrawTools peut imbriquer les tableaux (trous dans le polygone)
+        if (Array.isArray(latlngs[0])) latlngs = latlngs[0];
+        
+        return latlngs.map(function(ll) {
+          return window.map.latLngToLayerPoint(ll);
+        });
+      }
+    }
+    return null;
+  }
+
+  // --- FONCTION DE RESET ---
   function resetStyles(fieldsArray) {
     if (!fieldsArray) return;
     fieldsArray.forEach(function(f) {
       if (f && f.setStyle) {
         var team = f.options.team;
         var factionColor = (team === window.TEAM_ENL) ? window.COLORS[window.TEAM_ENL] : window.COLORS[window.TEAM_RES];
-        
         f.options.color = factionColor;
         f.options.weight = 2;
-        f.options.fillOpacity = 0.2;
-
-        f.setStyle({ 
-          color: factionColor,
-          weight: 2, 
-          opacity: 0.5, 
-          fillColor: factionColor,
-          fillOpacity: 0.2,
-          dashArray: null 
-        });
-
+        f.setStyle({ color: factionColor, weight: 2, opacity: 0.5, fillColor: factionColor, fillOpacity: 0.2, dashArray: null });
         if (f.redraw) f.redraw();
       }
     });
   }
 
-  // --- ILLUMINATION JAUNE ---
+  // --- ILLUMINATION ---
   function highlightField(field) {
     field.options.color = '#FFFF00'; 
-    field.setStyle({ 
-      color: '#FFFF00',
-      weight: 12, 
-      opacity: 1, 
-      fillColor: '#FFFF00',
-      fillOpacity: 0.4
-    });
+    field.setStyle({ color: '#FFFF00', weight: 12, opacity: 1, fillColor: '#FFFF00', fillOpacity: 0.4 });
     if (field.bringToFront) field.bringToFront();
   }
 
-  // --- CALCUL AU CLIC (AVEC ILLUMINATION PERSISTANTE) ---
-  function calculateAtPoint(ev) {
-    // On éteint les anciens fields avant d'allumer les nouveaux
-    resetStyles(currentHitFields);
-    currentHitFields = [];
-
-    var point = ev.layerPoint;
-    var res = 0, enl = 0;
-
-    for (var guid in window.fields) {
-      var field = window.fields[guid];
-      var rings = field._rings ? field._rings[0] : [];
-      if (!rings.length && field._latlngs) {
-        for (var i = 0; i < field._latlngs.length; i++) {
-          rings.push(window.map.latLngToLayerPoint(field._latlngs[i]));
-        }
-      }
-      if (window.pnpoly(rings, point)) {
-        currentHitFields.push(field);
-        highlightField(field);
-        
-        if (field.options.team === window.TEAM_ENL) enl++;
-        else if (field.options.team === window.TEAM_RES) res++;
-      }
-    }
-    
-    tooltip.innerHTML = "🔵 Res: " + res + " | 🟢 Enl: " + enl + " <span style='color:#ffce00; cursor:pointer; margin-left:5px; border:1px solid #ffce00; padding:0 3px;'>X</span>";
-    
-    // Ajout d'un clic sur le "X" de la bulle pour éteindre le jaune
-    tooltip.querySelector('span').onclick = function(e) {
-        e.stopPropagation();
-        resetStyles(currentHitFields);
-        currentHitFields = [];
-        tooltip.innerHTML = "Cliquez sur la carte";
-    };
-  }
-
-  // --- SCAN ÉCRAN (AVEC ILLUMINATION PERSISTANTE) ---
+  // --- SCAN ---
   layerCount.countAndHighlightVisible = function() {
     var bounds = window.map.getBounds();
+    var polyPoints = getPolygonPoints();
     var res = 0, enl = 0;
     var visibleFields = [];
 
     for (var guid in window.fields) {
       var field = window.fields[guid];
-      var isVisible = false;
       var locs = field.getLatLngs();
-      for (var i = 0; i < locs.length; i++) {
-        if (bounds.contains(locs[i])) { isVisible = true; break; }
+      var isTargeted = false;
+
+      if (polyPoints) {
+        // SI POLYGONE : On utilise pnpoly sur chaque sommet du field
+        for (var i = 0; i < locs.length; i++) {
+          var testPoint = window.map.latLngToLayerPoint(locs[i]);
+          if (window.pnpoly(polyPoints, testPoint)) {
+            isTargeted = true;
+            break;
+          }
+        }
+      } else {
+        // SI PAS DE POLYGONE : On utilise les bordures de l'écran
+        for (var k = 0; k < locs.length; k++) {
+          if (bounds.contains(locs[k])) {
+            isTargeted = true;
+            break;
+          }
+        }
       }
 
-      if (isVisible) {
+      if (isTargeted) {
         visibleFields.push(field);
         highlightField(field);
         if (field.options.team === window.TEAM_ENL) enl++;
@@ -118,18 +101,45 @@ function wrapper(plugin_info) {
       }
     }
 
+    var title = polyPoints ? "Analyse du Polygone" : "Analyse de l'Écran";
     var html = '<div style="padding: 15px; text-align: center; background: #222; color: #fff;">' +
                '<b style="color:#00bcff; font-size:1.1em;">🔵 Résistance :</b> ' + res + '<br>' +
                '<b style="color:#03fe03; font-size:1.1em;">🟢 Éclairés :</b> ' + enl + '<hr>' +
-               '<b style="font-size:1.3em;">Total : ' + (res + enl) + '</b></div>';
+               '<b style="font-size:1.3em;">Total : ' + (res + enl) + '</b><br>' +
+               '<small style="color:#aaa;">' + (polyPoints ? "Filtre DrawTools appliqué" : "Mode écran complet") + '</small></div>';
 
     window.dialog({ 
-        title: 'Analyse Zone', 
+        title: title, 
         html: html, 
         width: '250px',
         closeCallback: function() { resetStyles(visibleFields); }
     });
   };
+
+  // --- CALCUL AU CLIC ---
+  function calculateAtPoint(ev) {
+    resetStyles(currentHitFields);
+    currentHitFields = [];
+    var point = ev.layerPoint;
+    var res = 0, enl = 0;
+    for (var guid in window.fields) {
+      var field = window.fields[guid];
+      var rings = field._rings ? field._rings[0] : [];
+      if (!rings.length && field._latlngs) {
+        for (var i = 0; i < field._latlngs.length; i++) rings.push(window.map.latLngToLayerPoint(field._latlngs[i]));
+      }
+      if (window.pnpoly(rings, point)) {
+        currentHitFields.push(field);
+        highlightField(field);
+        if (field.options.team === window.TEAM_ENL) enl++;
+        else if (field.options.team === window.TEAM_RES) res++;
+      }
+    }
+    tooltip.innerHTML = "🔵 Res: " + res + " | 🟢 Enl: " + enl + " <span style='color:#ffce00; cursor:pointer; margin-left:5px; border:1px solid #ffce00; padding:0 3px;'>X</span>";
+    tooltip.querySelector('span').onclick = function(e) {
+        e.stopPropagation(); resetStyles(currentHitFields); currentHitFields = []; tooltip.innerHTML = "Cliquez sur la carte";
+    };
+  }
 
   // --- INTERFACE ---
   function setup() {
@@ -144,21 +154,13 @@ function wrapper(plugin_info) {
       options: { position: 'topleft' },
       onAdd: function (map) {
         var container = document.createElement('div'); container.className = 'leaflet-control-layer-count leaflet-bar';
-        var a = document.createElement('a'); a.title = 'Compter couches (clic)';
-        tooltip = document.createElement('div'); tooltip.className = 'lcount-tooltip';
-        tooltip.textContent = 'Cliquez sur la carte';
+        var a = document.createElement('a'); a.title = 'Couches au clic';
+        tooltip = document.createElement('div'); tooltip.className = 'lcount-tooltip'; tooltip.textContent = 'Cliquez sur la carte';
         a.appendChild(tooltip);
         a.addEventListener('click', function(e) {
           e.preventDefault();
-          if (this.classList.toggle('active-btn')) { 
-              map.on('click', calculateAtPoint); 
-              tooltip.style.display = 'block'; 
-          } else { 
-              map.off('click', calculateAtPoint); 
-              tooltip.style.display = 'none'; 
-              resetStyles(currentHitFields);
-              currentHitFields = [];
-          }
+          if (this.classList.toggle('active-btn')) { map.on('click', calculateAtPoint); tooltip.style.display = 'block'; }
+          else { map.off('click', calculateAtPoint); tooltip.style.display = 'none'; resetStyles(currentHitFields); currentHitFields = []; }
         });
         container.appendChild(a); return container;
       }
@@ -168,7 +170,7 @@ function wrapper(plugin_info) {
       options: { position: 'topleft' },
       onAdd: function (map) {
         var container = document.createElement('div'); container.className = 'leaflet-control-layer-visible leaflet-bar';
-        var a = document.createElement('a'); a.title = 'Scanner zone visible';
+        var a = document.createElement('a'); a.title = 'Scanner zone ou Polygone';
         a.addEventListener('click', function(e) { e.preventDefault(); layerCount.countAndHighlightVisible(); });
         container.appendChild(a); return container;
       }
