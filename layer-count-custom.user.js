ee864512fe59544dc5227f0afa93c1c28a68e273
jeanflo <jeanflorentcheveau@gmail.com>
2025-12-30 13:51:01 +0100

    Update layer-count-custom.user.js

--- layer-count-custom.user.js
+++ layer-count-custom.user.js
@@ -3,8 +3,10 @@
 // @name           IITC plugin: Layer count & Persistent Global Highlighter
 // @category       Info
 // @version        1.1.0
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-plugin-shardstorm.meta.js
+// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/iitc-pluin-layer-count-custom.user.js
 // @description    Surligne les fields de façon persistante au clic et sur l'écran
-// @id             layer-count-custom
+// @id             iitc-plugin-layer-count-custom
 // @match          https://intel.ingress.com/*
 // @match          https://intel-x.ingress.com/*
 // @grant          none
