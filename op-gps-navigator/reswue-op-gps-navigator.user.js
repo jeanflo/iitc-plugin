36873e7f8b6a6231b1ebde5c34fb98c18b0516da
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-04 22:34:37 +0100

    Update URLs for update and download in user script

--- op-gps-navigator/reswue-op-gps-navigator.user.js
+++ op-gps-navigator/reswue-op-gps-navigator.user.js
@@ -4,8 +4,8 @@
 // @category       Layer
 // @version        1.35.0.20260304-Tactical
 // @description    Extension Tactique : Option Moto, Fix routage temps réel (Vitesse piéton recalculée), Partage Telegram, Guidage GPS et Tri tactile.
-// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.meta.js
-// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.user.js
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.meta.js
+// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.user.js
 // @id             reswue-op-gps-navigator
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @match          https://intel.ingress.com/*
