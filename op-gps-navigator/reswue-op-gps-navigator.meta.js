367b78e0f591bd7c9048ca694bc1e98432a0199b
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-04 22:54:25 +0100

    Update reswue-op-gps-navigator.meta.js

--- op-gps-navigator/reswue-op-gps-navigator.meta.js
+++ op-gps-navigator/reswue-op-gps-navigator.meta.js
@@ -2,7 +2,7 @@
 // @name           RESWUE Navigator
 // @author         Z0mZ0m / Gemini
 // @category       Layer
-// @version        1.35.0.20260304-Tactical
+// @version        1.37.0.20260304-Tactical
 // @description    Extension Tactique : Option Moto, Fix routage temps réel (Vitesse piéton recalculée), Partage Telegram, Guidage GPS et Tri tactile.
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.user.js
