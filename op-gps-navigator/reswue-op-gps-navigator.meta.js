3c700bc17685f944014faf2e9082755479347451
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-04 20:43:52 +0100

    Add metadata for RESWUE: Operation GPS Navigator

---
+++ op-gps-navigator/reswue-op-gps-navigator.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           RESWUE: Operation GPS Navigator
+// @author         Z0mZ0m
+// @category       Layer
+// @version        1.29.0.20260303-Tactical
+// @description    Extension Tactique : Nombre de clés personnalisable, Popup HUD, Actions multiples, Routage. Bouton de ciblage déplacé dans une barre d'outils à gauche.
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.meta.js
+// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.user.js
+// @id             reswue-op-gps-navigator
+// @namespace      https://softspot.nl/ingress/
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
