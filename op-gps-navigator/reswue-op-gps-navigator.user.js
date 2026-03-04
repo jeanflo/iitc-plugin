0e7c4dbc2f0b5db411d90674355e00d394e4fccc
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-04 22:33:12 +0100

    Update user script ID for GPS navigator

--- op-gps-navigator/reswue-op-gps-navigator.user.js
+++ op-gps-navigator/reswue-op-gps-navigator.user.js
@@ -6,7 +6,7 @@
 // @description    Extension Tactique : Option Moto, Fix routage temps réel (Vitesse piéton recalculée), Partage Telegram, Guidage GPS et Tri tactile.
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.user.js
-// @id             reswue-navigator
+// @id             reswue-op-gps-navigator
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @match          https://intel.ingress.com/*
 // @grant          none
