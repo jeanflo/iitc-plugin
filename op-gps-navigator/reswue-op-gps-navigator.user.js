b83469e19b9827eac779a76d10f624a0dd9c4630
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-04 21:37:29 +0100

    Update version to 1.29.1.20260303-Tactical

--- op-gps-navigator/reswue-op-gps-navigator.user.js
+++ op-gps-navigator/reswue-op-gps-navigator.user.js
@@ -2,7 +2,7 @@
 // @name           RESWUE: Operation GPS Navigator
 // @author         Z0mZ0m
 // @category       Layer
-// @version        1.29.0.20260303-Tactical
+// @version        1.29.1.20260303-Tactical
 // @description    Extension Tactique : Nombre de clés personnalisable, Popup HUD, Actions multiples, Routage. Bouton de ciblage déplacé dans une barre d'outils à gauche.
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.user.js
