86d9c74f6b597beb70e8d9a53a4ed7173157583e
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-07 12:30:27 +0100

    Update reswue-op-gps-navigator.meta.js

--- op-gps-navigator/reswue-op-gps-navigator.meta.js
+++ op-gps-navigator/reswue-op-gps-navigator.meta.js
@@ -2,11 +2,11 @@
 // @name           RESWUE Navigator
 // @author         Z0mZ0m / Gemini
 // @category       Layer
-// @version        1.40.0.20260304-Tactical
-// @description    Extension Tactique : Option Moto, Fix routage temps réel (Vitesse piéton recalculée), Partage Telegram, Guidage GPS et Tri tactile.
-// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.meta.js
-// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.user.js
-// @id             reswue-op-gps-navigator
+// @version        1.39.0.20260307-Tactical
+// @description    Extension Tactique : Fix Forçage Voix Android, HUD mobile, Telegram Auto, Tri tactile.
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.meta.js
+// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.user.js
+// @id             reswue-navigator
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @match          https://intel.ingress.com/*
 // @grant          none
