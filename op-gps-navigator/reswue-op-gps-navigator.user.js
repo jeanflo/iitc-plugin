486cefabf61fb5645c0f0ab9918f043b8d7c6350
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-07 12:38:29 +0100

    Update reswue-op-gps-navigator.user.js

--- op-gps-navigator/reswue-op-gps-navigator.user.js
+++ op-gps-navigator/reswue-op-gps-navigator.user.js
@@ -4,8 +4,8 @@
 // @category       Layer
 // @version        1.38.0.20260307-Tactical
 // @description    Extension Tactique : Fix Forçage Voix Android, HUD mobile, Telegram Auto, Tri tactile.
-// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.meta.js
-// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/iitc-plugin-op-gps-navigator.user.js
+// @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.meta.js
+// @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/op-gps-navigator/reswue-op-gps-navigator.user.js
 // @id             reswue-navigator
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @match          https://intel.ingress.com/*
