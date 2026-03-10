b5b659e19791d6bf33e188323c41e6090be83daa
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-10 07:46:54 +0100

    Update iitc-plugin-favorite-portal.meta.js

--- Favorite-Portal/iitc-plugin-favorite-portal.meta.js
+++ Favorite-Portal/iitc-plugin-favorite-portal.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name           IITC plugin: Favorite portal details (Toolbar Integration)
-// @author         DanielOnDiordna / Z0mZ0m
+// @author         DanielOnDiordna / Gemini
 // @category       Info
-// @version        1.2.27.20260303-Update
+// @version        1.2.28.20260310-Update
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/Favorite-Portal/iitc-plugin-favorite-portal.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/Favorite-Portal/iitc-plugin-favorite-portal.user.js
-// @description    [1.2.27] Bouton d'ajout aux favoris déplacé dans une barre d'outils Leaflet à gauche pour plus d'ergonomie.
+// @description    [1.2.28] L'icône d'alerte (⚠️) est désormais cliquable et ouvre le portail directement dans Ingress Prime.
 // @id             iitc-plugin-favorite-portal-details
 // @namespace      https://softspot.nl/ingress/
 // @match          https://intel.ingress.com/*
