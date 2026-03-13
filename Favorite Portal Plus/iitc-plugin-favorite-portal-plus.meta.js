b452623d75cf6f4a06bab5684095091f967b791a
jeanflo <jeanflorentcheveau@gmail.com>
2026-03-13 23:49:03 +0100

    Update iitc-plugin-favorite-portal-plus.meta.js

--- Favorite Portal Plus/iitc-plugin-favorite-portal-plus.meta.js
+++ Favorite Portal Plus/iitc-plugin-favorite-portal-plus.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name           IITC plugin: Favorite portal details (V6.6.4 Secure Fix)
+// @name           IITC plugin: Favorite portal details (V6.6.5 Secure Fix)
 // @author         Z0mz0m
 // @category       Info
-// @version        6.6.4.20260313-Ultimate
+// @version        6.6.5.20260313-Ultimate
 // @updateURL      https://github.com/jeanflo/iitc-plugin/raw/refs/heads/main/Favorite%20Portal%20Plus/iitc-plugin-favorite-portal-plus.meta.js
 // @downloadURL    https://github.com/jeanflo/iitc-plugin/raw/refs/heads/main/Favorite%20Portal%20Plus/iitc-plugin-favorite-portal-plus.user.js
-// @description    [6.6.4] Sécurisé pour la Résistance uniquement.
+// @description    [6.6.5] Sécurisé pour la Résistance uniquement.
 // @id             iitc-plugin-favorite-portal-details-plus
 // @namespace      https://softspot.nl/ingress/
 // @match          https://intel.ingress.com/*
