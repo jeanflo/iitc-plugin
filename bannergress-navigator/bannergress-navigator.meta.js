0686072e395e06ced819c902efbd7a7acd8100c1
zomzom <jeanflorentcheveau@gmail.com>
2026-06-14 09:21:28 +0200

    Update bannergress-navigator.meta.js

--- bannergress-navigator/bannergress-navigator.meta.js
+++ bannergress-navigator/bannergress-navigator.meta.js
@@ -2,7 +2,7 @@
 // @name           IITC plugin: Bannergress Navigator
 // @author         Z0mZ0m
 // @category       Layer
-// @version        1.1.1
+// @version        1.1.2
 // @description    Navigation piétonne automatisée pour les fresques Bannergress avec auto-validation et Deep Links Ingress Prime.
 // @updateURL      https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/bannergress-navigator/bannergress-navigator.meta.js
 // @downloadURL    https://raw.githubusercontent.com/jeanflo/iitc-plugin/refs/heads/main/bannergress-navigator/bannergress-navigator.user.js
@@ -10,4 +10,5 @@
 // @namespace      https://github.com/jeanflo/iitc-plugin
 // @match          https://intel.ingress.com/*
 // @grant          none
+// @icon64         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZmlsbD0iI2ZmM2IzMCIgZD0iTTE0LjQgNkwxNCA0SDV2MTdoMnYtN2g1LjZsLjQgMmg3VjZ6Ii8+PC9zdmc+
 // ==/UserScript==
