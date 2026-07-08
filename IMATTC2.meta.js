8d6d3baa065dfd24b92a16655be9cc26f9552a10
zomzom <jeanflorentcheveau@gmail.com>
2026-07-08 13:59:52 +0200

    Add metadata for IMATTC user script

---
+++ IMATTC2.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         IMATTC
+// @version      1.8.2
+// @description  A usability overhaul for the Ingress Mission Authoring Tool
+// @author       @Chyld314
+// @match        https://mission-author-dot-betaspike.appspot.com/
+// @match        https://mission-author-dot-betaspike.appspot.com/edit*
+// @match        http://missions.ingress.com/
+// @match        http://missions.ingress.com/edit*
+// @match        https://missions.ingress.com/
+// @match        https://missions.ingress.com/edit*
+// @grant        none
+// @downloadURL	 https://github.com/jeanflo/iitc-plugin/blob/main/IMATTC2.user.js
+// @updateURL	 https://github.com/jeanflo/iitc-plugin/blob/main/IMATTC2.meta.js
+// ==/UserScript==
